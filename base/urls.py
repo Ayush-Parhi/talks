78718b0d166d23f13c183b2112ef323c3f6acd1a
Ayush-Parhi <ayushparhi7@gmail.com>
2023-07-03 04:40:50 +0530

    changes

--- base/urls.py
+++ base/urls.py
@@ -13,6 +13,3 @@ urlpatterns = [
     path('delete_member/', views.deleteMember),
 
 ]   
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mychat/settings.py
+++ mychat/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'mychat.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-  #  'default': {
-   #     'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
@@ -129,6 +129,3 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
